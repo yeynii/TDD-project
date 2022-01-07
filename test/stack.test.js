e2fca67a4db14d18a15ac02bc980fa98bca855ad
yeynii <yeynii@gmail.com>
2022-01-08 01:37:58 +0900

    peek, size 메서드 테스트 추가

--- test/stack.test.js
+++ test/stack.test.js
@@ -11,16 +11,34 @@ describe("stack test", () => {
     stack.push(1);
     stack.push(2);
     expect(stack.array).toStrictEqual([1, 2]);
+    expect(stack.size()).toBe(2);
   });
 
-  it("pop", () => {
-    stack.push(1);
-    expect(stack.pop()).toBe(1);
+  describe("pop", () => {
+    it("empty error", () => {
+      expect(() => stack.pop()).toThrow('빈 스택');
+      expect(() => stack.pop()).toThrow();
+      expect(() => stack.pop()).toThrow(new Error('빈 스택입니다.'));
+    });
+
+    it("allows to pop", () => {
+        stack.push(1);
+        expect(stack.pop()).toBe(1);
+        expect(stack.size()).toBe(0);
+    })
   });
 
-  it("empty error", () => {
-    expect(() => stack.pop()).toThrow('빈 스택');
-    expect(() => stack.pop()).toThrow();
-    expect(() => stack.pop()).toThrow(new Error('빈 스택에서 pop 할 수 없습니다.'));
-  });
+  describe('peek', () => {
+    it("empty error", () => {
+      expect(() => stack.peek()).toThrow(new Error('빈 스택입니다.'));
+    });
+
+    it("allows to peek", () => {
+        stack.push(1);
+        expect(stack.peek()).toBe(1);
+        expect(stack.size()).toBe(1);
+    })
+
+  })
+
 });
