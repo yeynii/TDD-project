52a2d53d00474b0a2c5efa286c06fac7bf0f9936
yeynii <yeynii@gmail.com>
2022-01-08 01:21:02 +0900

    테스트 케이스 추가 및 오류 수정

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,16 +1,26 @@
-const Stack = requied('../src/stack.js');
+const Stack = require("../src/stack.js");
 
-describe('stack test', () => {
-    let stack;
-    beforeEach(() => {
-        stack = new Stack();
-    })
-    it('push', () => {
-        stack.push(1);
-        expect(stack[0]).toBe(1);
-    });
-    it('pop', () => {
-        stack.push(2);
-        expect(stack.pop()).toBe(2);
-    });
+describe("stack test", () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it("push", () => {
+    stack.push(1);
+    stack.push(2);
+    expect(stack.array).toStrictEqual([1, 2]);
+  });
+
+  it("pop", () => {
+    stack.push(1);
+    expect(stack.pop()).toBe(1);
+  });
+
+  it("empty error", () => {
+    expect(() => stack.pop()).toThrow('빈 스택');
+    expect(() => stack.pop()).toThrow();
+    expect(() => stack.pop()).toThrow(new Error('빈 스택에서 pop 할 수 없습니다.'));
+  });
 });
